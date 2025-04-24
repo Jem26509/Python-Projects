7666805f6ec473c9d4f65e60c1220d7df05e1cd1
Jem26509 <jem26509@hotmail.com>
2025-04-24 13:26:05 -0500

    2025-04-24 Python

--- Textos/Prueba.py
+++ Textos/Prueba.py
@@ -5,31 +5,31 @@ import sys
  
 if len(sys.argv) > 1:
     for i in sys.argv:
-       pdf_directory = i
+       directory = i
 else:
-    pdf_directory = input('Directorio: ') #'D:\8 - Linkedin\BI'
-Carpeta = os.chdir(pdf_directory)
+    directory = input('Directorio: ') #'D:\8 - Linkedin\BI'
+Carpeta = os.chdir(directory)
 
 for Nombre in os.listdir():
     print('1', Nombre)
     #print(os.path.splitext(Nombre))
     texto, extension = os.path.splitext(Nombre)
-    if extension == '.pdf':
-      texto = Nombre.replace('_',' ')
-      texto = texto.replace('  ',' ')
-      #print('2', texto)
-
-      nombre = texto.split(" 173")
-      #print('3', nombre[0])
-      if len(nombre) == 1:
-         texto = nombre[0]
-         #print('4', texto)
-
-      if len(nombre) == 2:
-          texto = nombre[0] + extension
-          #print('4', texto)
     
-      texto = Capital.Capital(texto).capital()
-      print('9', texto)
-      os.rename(Nombre,texto)
+    texto = Nombre.replace('_',' ')
+    texto = texto.replace('  ',' ')
+    #print('2', texto)
+
+    nombre = texto.split(" 173")
+    #print('3', nombre[0])
+    if len(nombre) == 1:
+        texto = nombre[0]
+        #print('4', texto)
+
+    if len(nombre) == 2:
+        texto = nombre[0] + extension
+        #print('4', texto)
+
+    texto = Capital.Capital(texto).capital()
+    print('9', texto)
+    os.rename(Nombre,texto)
 print("FIN")
